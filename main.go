e34587f686b7deed924a756ad82190c1342bc0d2
Alex Russell <holcron@Alexs-MBP.home>
2017-03-21 17:51:48 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+
+	//"github.com/elastic/beats/libbeat/beat"
+
+	//"github.com/red-two/lsbeat/beater"
+)
+
+func main() {
+	  //apply run path "." without argument.
+    if len(os.Args) == 1 {
+        listDir(".")
+    } else {
+        listDir(os.Args[1])
+    }
+}
+
+func listDir(dirFile string) {
+    files, _ := ioutil.ReadDir(dirFile)
+    for _, f := range files {
+        t := f.ModTime()
+        fmt.Println(f.Name(), dirFile+"/"+f.Name(), f.IsDir(), t, f.Size())
+        if f.IsDir() {
+            listDir(dirFile + "/" + f.Name())
+        }
+    }
+}
